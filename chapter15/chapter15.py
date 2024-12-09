8a0f7731502349fe15ea60d55a04e9437ac9671a
xiname <xinametravel@qq.com>
2024-12-09 09:52:05 +0800

    chapter 15 ending.
    Signed-off-by: xiname <xinametravel@qq.com>

--- chapter15/chapter15.py
+++ chapter15/chapter15.py
@@ -46,5 +46,23 @@
 
 # 交换位置
 
-loc = ([1, 2, 3, 4], [11, 12, 13, 14])
-print(loc, loc[::-1], end='\n')
+# loc = ([1, 2, 3, 4], [11, 12, 13, 14])
+# print(loc, loc[::-1], sep="\n")
+
+
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv2.imread("./pic/lena4.bmp", 0)
+template = cv2.imread("./pic/lena4Temp.bmp", 0)
+w, h = template.shape[::-1]
+res = cv2.matchTemplate(img, template, cv2.TM_CCOEFF_NORMED)
+threshold = 0.9
+loc = np.where(res >= threshold)
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(img, pt, (pt[0] + w, pt[1] + h), 255, 1)
+plt.imshow(img, cmap='gray')
+plt.xticks([])
+plt.yticks([])
+plt.show()
---
+++ chapter16/chapter16.py
@@ -0,0 +1,8 @@
+# _*_ coding: utf-8 _*_
+#
+# Copyright (C) 2024 - 2024 xiname All Rights Reserved 
+#
+# @Time    : 2024/12/9 09:52
+# @Author  : xiname
+# @File    : chapter16.py
+# @IDE     : PyCharm
